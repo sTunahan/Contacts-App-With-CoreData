fd562bc3dea205945a2539ccf68fbea543ed2c29
YedekTunahan <elamtraxemon@gmail.com>
2022-01-25 13:26:04 +0300

    Initial Commit

---
+++ Persons_App_Design/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Persons_App_Design
+//
+//  Created by asuftech on 25.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
